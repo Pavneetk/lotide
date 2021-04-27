70d9a666a7e40a3ce1efbf50c344731d3beb0f25
Pavneet Kang <pavneet.kang2@gmail.com>
2021-04-27 00:32:25 +0000

    Calling a function in a function, Head Array function

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: [${actual}] === [${expected}]`);
+    return true;
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+    return false;
+  }
+};
+
+const head = function(array1) {
+  return array1[0];
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
