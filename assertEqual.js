1c06318eaf7502213d06055bf0ce3b7533442d98
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-03 21:58:23 +0000

    Separating functions and tests out into files using module export and require

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✔️✔️✔️ Assertion Passed: [" + actual + "] === [" + expected + "]");
+    console.log(`✔️  Assertion Passed: [${actual}] === [${expected}]`);
     return true;
   } else {
-    console.log("❌❌❌ Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
     return false;
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
