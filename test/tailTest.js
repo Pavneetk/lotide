8a49559399af6c823cb7c914bdf84f64cc9581eb
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-04 00:36:47 +0000

    Converting test code into mocha chai framework describe and it functions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+});
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const words1 = [];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(tail(words1));
-assertEqual(words.length, 3); // original array should still have 3 elements!
